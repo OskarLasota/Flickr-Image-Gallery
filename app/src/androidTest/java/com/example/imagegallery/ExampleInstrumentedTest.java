e68df481a9e91d14af714ac1e1e03df89b86ba85
Oskar Lasota <55768328+OskarLasota@users.noreply.github.com>
2020-04-12 16:33:45 +0100

    chore: testing for api

--- app/src/androidTest/java/com/example/imagegallery/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/imagegallery/ExampleInstrumentedTest.java
@@ -1,10 +1,13 @@
 package com.example.imagegallery;
 
+import android.app.Application;
 import android.content.Context;
 
 import com.example.imagegallery.adapters.ViewAdapter;
 import com.example.imagegallery.models.FlickrImage;
+import com.example.imagegallery.viewmodels.MainActivityViewModel;
 
+import androidx.lifecycle.ViewModelProvider;
 import androidx.test.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
@@ -20,7 +23,6 @@ import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
- *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
--- app/src/test/java/com/example/imagegallery/ExampleUnitTest.java
+++ app/src/test/java/com/example/imagegallery/ExampleUnitTest.java
@@ -1,18 +1,22 @@
 package com.example.imagegallery;
 
-import android.view.LayoutInflater;
-import android.view.View;
-
 import com.example.imagegallery.adapters.ViewAdapter;
 import com.example.imagegallery.models.FlickrImage;
-
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
 import static org.mockito.Mockito.mock;
 
 /**
@@ -20,7 +24,31 @@ import static org.mockito.Mockito.mock;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(MockitoJUnitRunner.class)
+
 public class ExampleUnitTest {
+
+    private final String key = BuildConfig.ApiKey;
+    private final String keyword = "kitten";
+
+
+
+    @Test
+    public void checking_api_availability() throws Exception{
+        InputStream response = createConnection(
+                "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="+key+"&tags="+keyword+"&page=1&format=json&nojsoncallback=1");
+
+        assert convertResponse(response).length() > 0;
+    }
+
+    @Test
+    public void checking_apikey_availability() throws Exception{
+        String randomstring = "sfsgasdf%";
+        InputStream response = createConnection(
+                "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="+key+randomstring+"&tags="+keyword+"&page=1&format=json&nojsoncallback=1");
+
+        assertTrue(convertResponse(response).contains("Invalid API Key (Key has invalid format)"));
+    }
 
     @Test
     public void initializing_recycler_adapter(){
@@ -30,6 +58,23 @@ public class ExampleUnitTest {
         MainActivity activity = mock(MainActivity.class);
         ViewAdapter loginPresenter = new ViewAdapter(activity, list);
         Assert.assertEquals(1, loginPresenter.getItemCount());
+    }
+
+
+
+    private InputStream createConnection(String url) throws Exception{
+        URLConnection connection = new URL(url).openConnection();
+        return connection.getInputStream();
+    }
+
+    private String convertResponse(InputStream stream) throws Exception{
+        StringBuffer buffer = new StringBuffer();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream, Charset.defaultCharset()))) {
+            for (String line; (line = reader.readLine()) != null; ) {
+                buffer.append(line);
+            }
+        }
+        return buffer.toString();
     }
 
 
